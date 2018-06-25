00d93d727a010c9c82eaf56a86f60f05e58d30fb
danchurch <danchurchthomas@gmail.com>
2018-06-25 00:44:28 -0700

    sleep

--- CSrev/biomsetup.R
+++ CSrev/biomsetup.R
@@ -9,6 +9,7 @@ library('igraph')
 library('ecodist')
 library('ade4')
 library('png')
+library('randomcoloR')
 
 biom95 <- import_biom('combo_otu_wMeta.biom', parseFunction=parse_taxonomy_greengenes)
 
